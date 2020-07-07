5d3c8f82e44520051762e8683614444b514bf849
George Hardcastle <th3g0r327.gh@gmail.com>
2020-07-07 16:59:44 +0000

    Automatically backed up by Learn

--- variable.rb
+++ variable.rb
@@ -1,9 +1,4 @@
  # Define a local variable called 'greeting',
  # give it the value of the string "Hello World"
-greeting = get_variable_from_file('./variable.rb', "greeting")
-  expect(greeting).to eq("Hello World")
-
-
-
 
 
